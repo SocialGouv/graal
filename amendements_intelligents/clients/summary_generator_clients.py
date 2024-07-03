8d3166d91c15b89ed0c42af26ac620ec68dc2688
Victor DEGLIAME <victor.degliame@gmail.com>
2024-07-04 01:45:51 +0200

    refactor: Summary generation into neat python modules
    vidy-br: refactor-summary-generation

--- amendements_intelligents/clients/summary_generator_clients.py
+++ amendements_intelligents/clients/summary_generator_clients.py
@@ -67,6 +67,36 @@ class SummaryGeneratorOllamaClient:
 
         return prompt
 
+    def build_prompt_3(self, content: TxtContent):
+        prompt = f"""
+        Contexte : Tu es un juriste spécialisé dans le droit de la sécurité sociale en France. Ta tâche est de résumer un amendement et de rester politiquement correct.
+
+        Instruction: Résume l'amendement en français en une phrase de 10 à 20 mots. Le résumé doit être concis, neutre, commencer par un verbe à l'infinitif, inclure les taux (mais pas d'autres chiffres), ne pas inventer de chiffres qui ne figurent pas dans l'amendement, ne pas utiliser d'adjectifs, et exclure les justifications de l'amendement. Assume que le lecteur connaît le contexte. Utilise des acronymes sans les expliciter. Si l'amendement concerne une expérimentation ou un rapport, mentionne-le dans le résumé. N'ajoute pas de notes expliquant le résumé.
+
+        Exemples :
+
+        ```
+        Amendement :
+        Cet amendement vise à mettre fin à une situation soumettant à des cotisations forfaitaires les chefs d’exploitation agricole ou d’entreprise agricole bénéficiaires du RSA. Il les soustrait ainsi au paiement des cotisations accidents du travail, invalidité des conjoints collaborateurs et de la cotisation forfaitaire due au titre des indemnités journalières maladie.
+        Face à la situation de détresse de nombreux agriculteurs, cet amendement vise à permettre à ceux en situation de grande précarité d’être soulagés du paiement de cotisations qui viennent renforcer les difficultés financières dans lesquels ceux-ci peuvent se trouver.
+        Il serait ainsi mis fin à une situation juridique instable où nombre d’agriculteurs se voient réclamés par la mutualité sociale agricole des cotisations sociales forfaitaires qu’ils ne sont pas en mesure de payer.
+        Résumé :
+        Exonérer de toute cotisation forfaitaire les exploitants agricoles bénéficiaire du revenu de solidarité active
+
+        Amendement :
+        Demande de rapport du Gouvernement au Parlement sur le financement de la sécurité sociale à Mayotte et l’opportunité d’une accélération de la trajectoire de convergence des cotisations et contributions sociales.
+        Résumé :
+        Demander un rapport sur le financement de la sécurité sociale à Mayotte et la convergence des cotisations sociales.
+        ```
+
+        Amendement :
+        {content}
+
+        Résumé :
+        """
+
+        return prompt
+
     def generate_response(self, prompt: Prompt):
         for i in range(self.MAX_RETRIES):
             try:
--- amendements_intelligents/utils/sheet_data_extractor.py
+++ amendements_intelligents/utils/sheet_data_extractor.py
@@ -4,12 +4,13 @@ import pandas as pd
 class SheetDataExtractor:
     def __init__(self, file_path):
         self.file_path = file_path
-        self.xlsm_data = pd.read_excel(file_path, sheet_name=None)
-        self.sheet_names = self.xlsm_data.keys()
+        self.excel_data = pd.read_excel(file_path, sheet_name=None)
+        self.sheet_names = self.excel_data.keys()
+        print(f"Sheet names: {self.sheet_names}")
 
     def extract_sheet_data(self, sheet_name):
         if sheet_name in self.sheet_names:
-            sheet_data = self.xlsm_data[sheet_name]
+            sheet_data = self.excel_data[sheet_name]
             df = pd.DataFrame(sheet_data.values, columns=sheet_data.iloc[0])
             df = df.iloc[1:]
             return df
@@ -17,8 +18,8 @@ class SheetDataExtractor:
             print(f"Sheet '{sheet_name}' not found.")
             return None
 
-    def save_sheet_data_as_json(self, sheet_name, output_path):
+    def save_sheet_data_as_json(self, sheet_name, output_path, orient="records"):
         df = self.extract_sheet_data(sheet_name)
         if df is not None:
-            df.to_json(output_path, orient="records", index=False)
+            df.to_json(output_path, orient=orient, index=False)
             print(f"Sheet '{sheet_name}' data saved as JSON: {output_path}")
