dc40bba0c9b198ee9c52c1e1c92264cb31631a18
Victor DEGLIAME <victor.degliame@gmail.com>
2024-07-17 15:53:24 +0200

    feat: Add populate_allotments.py script that will run the full allotment flow for a PLFSS
    vidy-br: populate-allotments

---
+++ amendements_intelligents/data_handlers/plfss_sheet_data_loader.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+
+class PLFSSSheetDataLoader:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.excel_data = pd.read_excel(file_path, sheet_name=None)
+        self.sheet_names = self.excel_data.keys()
+        print(f"Sheet names in the excel file: {self.sheet_names}")
+
+    def extract_sheet_data(self, sheet_name):
+        print(f'Extracting PLFSS data from sheet "{sheet_name}"')
+        if sheet_name in self.sheet_names:
+            sheet_data = self.excel_data[sheet_name]
+        else:
+            print(f"Sheet '{sheet_name}' not found.")
+        return sheet_data
+
+    def show_exploratory_analysis(self, plfss_df: pd.DataFrame) -> None:
+        # Get the number of rows and columns
+        num_rows, num_cols = plfss_df.shape
+        print(f"Number of rows: {num_rows}")
+        print(f"Number of columns: {num_cols}")
+
+        # Get the column names
+        column_names = plfss_df.columns.tolist()
+        print("Column names:")
+        print(column_names)
+
+        # Calculate the ratio of missing values for each column
+        missing_ratio = plfss_df.isnull().mean()
+
+        # Print the missing ratio for each column
+        print("Missing value ratio for each column:")
+        print(missing_ratio * 100)
+
+        # Calculate the length of each value in the 'Exposé amdt' column
+        lengths = plfss_df["Exposé amdt"].dropna().apply(len)
+
+        # Display the minimum, mean, and maximum length
+        print("Analysis of 'Exposé amdt':")
+        print(f"Minimum length: {lengths.min()}")
+        print(f"Mean length: {lengths.mean()}")
+        print(f"Maximum length: {lengths.max()}")
--- amendements_intelligents/populate_allotments.py
+++ amendements_intelligents/populate_allotments.py
@@ -3,133 +3,64 @@ import time
 
 import pandas as pd
 from pydantic import FilePath
-from sklearn.cluster import DBSCAN
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 
-from amendements_intelligents.types import IntIndex
-from amendements_intelligents.utils.plfss_text_processor import PLFSSTextProcessor
+from amendements_intelligents.clustering.plfss_cluster_finder import PLFSSClusterFinder
+from amendements_intelligents.data_handlers.plfss_allotment_updater import (
+    PLFSSAllotmentUpdater,
+)
+from amendements_intelligents.data_handlers.plfss_data_processor import (
+    PLFSSDataProcessor,
+)
 
 
-class PLFSSDataProcessor:
-    def __init__(self, input_file: FilePath):
+class PLFSSAllotmentPopulator:
+    def __init__(self, input_file: FilePath) -> None:
         self.input_file = input_file
-        self.columns_to_output = ["Numéro", "Allotissement", "Exposé des motifs"]
-        self.amendments_df = None
-        self.preprocessed_amendments_df = None
 
-    def load_data(self) -> None:
-        self.amendments_df = pd.read_json(self.input_file)
-        self.amendments_df["Allotissement"] = None
+    def process(self) -> pd.DataFrame:
+        # Data processing
+        plfss_data_processor = PLFSSDataProcessor(input_file=self.input_file)
+        plfss_data_processor.load_plfss()
+        preprocessed_amendments_df = plfss_data_processor.preprocess_plfss()
 
-    def preprocess_data(self) -> None:
-        self.preprocessed_amendments_df = PLFSSTextProcessor.preprocess_df(
-            self.amendments_df
+        # Clustering
+        cluster_finder = PLFSSClusterFinder(
+            preprocessed_amendments_df=preprocessed_amendments_df
         )
-        print("PLFSS loaded for processing")
+        final_clusters = cluster_finder.find_similarity_clusters(eps=0.01)
+        final_clusters = cluster_finder.refine_clusters_with_exact_match(
+            threshold=0.0001
+        )
 
-
-class PLFSSClusterFinder:
-    def __init__(self, preprocessed_amendments_df: pd.DataFrame):
-        self.preprocessed_amendments_df = preprocessed_amendments_df
-        self.vectors = None
-        self.distance_matrix = None
-        self.final_clusters = None
-
-    def vectorize_data(self) -> None:
-        print("Converting strings to TF-IDF vectors...")
-        strings = self.preprocessed_amendments_df["Exposé des motifs"].tolist()
-        vectorizer = TfidfVectorizer()
-        self.vectors = vectorizer.fit_transform(strings)
-
-    def compute_distance_matrix(self) -> None:
-        print("Computing cosine similarity matrix...")
-        similarity_matrix = cosine_similarity(self.vectors)
-        distance_matrix = 1 - similarity_matrix
-        distance_matrix[distance_matrix < 0] = 0  # Ensure no negative values
-        self.distance_matrix = distance_matrix
-
-    def find_clusters(self) -> None:
-        print("Finding clusters...")
-        dbscan = DBSCAN(metric="precomputed", eps=0.5, min_samples=2)
-        clusters = dbscan.fit_predict(self.distance_matrix)
-
-        # Extract clusters
-        clustered_strings = {}
-        for idx, label in enumerate(clusters):
-            if label == -1:  # Ignore noise points
-                continue
-            if label not in clustered_strings:
-                clustered_strings[label] = []
-            clustered_strings[label].append(idx)
-
-        # Filter out singleton clusters
-        self.final_clusters = [
-            cluster for cluster in clustered_strings.values() if len(cluster) > 1
-        ]
-        print("Number of clusters:", len(self.final_clusters))
-
-
-class AllotmentUpdater:
-    def __init__(
-        self,
-        amendments_df: pd.DataFrame,
-        preprocessed_amendments_df: pd.DataFrame,
-        final_clusters: list[list[IntIndex]],
-    ) -> None:
-        self.amendments_df = amendments_df
-        self.preprocessed_amendments_df = preprocessed_amendments_df
-        self.final_clusters = final_clusters
-
-    def update_allotissement(self) -> None:
-        for cluster_indices in self.final_clusters:
-            cluster_numeros = sorted(
-                self.preprocessed_amendments_df.iloc[idx]["Numéro"]
-                for idx in cluster_indices
-            )
-            cluster_numeros_str = ",".join(map(str, cluster_numeros))
-            for idx in cluster_indices:
-                amendment_numero = self.preprocessed_amendments_df.iloc[idx]["Numéro"]
-                lecture = self.preprocessed_amendments_df.iloc[idx]["Lecture"]
-                mask = (self.amendments_df["Numéro"] == amendment_numero) & (
-                    self.amendments_df["Lecture"] == lecture
-                )
-                self.amendments_df.loc[mask, "Allotissement"] = cluster_numeros_str
+        # Result processing
+        allotment_updater = PLFSSAllotmentUpdater(
+            amendments_df=plfss_data_processor.amendments_df,
+            preprocessed_amendments_df=plfss_data_processor.preprocessed_amendments_df,
+            final_clusters=final_clusters,
+        )
+        return allotment_updater.update_allotissement()
 
 
 def main():
     start_time = time.time()
     DATA_FOLDER = os.getenv("DATA_FOLDER", "data")
     INPUT_FILE = f"{DATA_FOLDER}/PLFSS 2024.json"
-    OUTPUT_FILE = f"{DATA_FOLDER}/amendments_with_allotissement.xlsx"
+    OUTPUT_FILE = f"{DATA_FOLDER}/amendments_with_allotments.xlsx"
+    COLUMNS_TO_OUTPUT = [
+        "Lecture",
+        "Num amdt",
+        "Num article",
+        "Allotissement",
+        "Corps amdt",
+        "Exposé amdt",
+    ]
 
-    # Data processing
-    plfss_data_processor = PLFSSDataProcessor(input_file=INPUT_FILE)
-    plfss_data_processor.load_data()
-    plfss_data_processor.preprocess_data()
+    allotment_populator = PLFSSAllotmentPopulator(input_file=INPUT_FILE)
+    amendments_df = allotment_populator.process()
 
-    # Clustering
-    cluster_finder = PLFSSClusterFinder(
-        preprocessed_amendments_df=plfss_data_processor.preprocessed_amendments_df
-    )
-    cluster_finder.vectorize_data()
-    cluster_finder.compute_distance_matrix()
-    cluster_finder.find_clusters()
+    amendments_df[COLUMNS_TO_OUTPUT].to_excel(OUTPUT_FILE, index=False)
+    print(f"Saved result in {OUTPUT_FILE}\n")
 
-    # Result processing
-    allotment_updater = AllotmentUpdater(
-        amendments_df=plfss_data_processor.amendments_df,
-        preprocessed_amendments_df=plfss_data_processor.preprocessed_amendments_df,
-        final_clusters=cluster_finder.final_clusters,
-    )
-
-    allotment_updater.update_allotissement()
-    allotment_updater.amendments_df[plfss_data_processor.columns_to_output].to_excel(
-        OUTPUT_FILE, index=False
-    )
-    print(f"Saved result in {OUTPUT_FILE}")
-
-    # Print execution time
     end_time = time.time()
     execution_time = end_time - start_time
     print("Script execution time:", execution_time, "seconds")
