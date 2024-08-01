604731a5357603a00fe3ad5a2257d7341112a0f0
Victor DEGLIAME <victor.degliame@gmail.com>
2024-08-01 13:31:20 +0200

    tests: Add tests for SimilarityFinder
    vidy-br: misc-tests

---
+++ amendements_intelligents/clustering/similarity_finder.py
@@ -0,0 +1,64 @@
+import pandas as pd
+
+from amendements_intelligents.types import IntIndex
+from amendements_intelligents.utils.content_similarity_evaluator import (
+    ContentSimilarityEvaluator,
+)
+
+
+class SimilarityFinder:
+    def __init__(
+        self, old_amendments_df: pd.DataFrame, new_amendments_df: pd.DataFrame
+    ):
+        self.old_amendments_df = old_amendments_df
+        self.new_amendments_df = new_amendments_df
+        self.similar_doc_indices = None
+
+    def prefilter_similar_docs(
+        self, column_used_for_comparison: str = "Exposé amdt"
+    ) -> dict[IntIndex, list[IntIndex]]:
+        """
+        Pre-filters similar documents based on a TF-IDF comparison of the `column_used_for_comparison` in the old and new amendments.
+
+        Internally, it saves a dictionary with keys being the index of the new amendment and values being a list of indices of the old amendments that are similar. It will be used to find the best matches later on.
+
+        Return: The dictionnary mentionned above.
+        """
+        print("Pre-filtering similar documents for optimization...")
+        self.similar_doc_indices = ContentSimilarityEvaluator.tf_idf_filtering(
+            documents_to_search=self.old_amendments_df[
+                column_used_for_comparison
+            ].tolist(),
+            documents_to_filter=self.new_amendments_df[
+                column_used_for_comparison
+            ].tolist(),
+            threshold=0.7,
+        )
+        list_lengths = [len(docs) for docs in self.similar_doc_indices.values()]
+        average_length = sum(list_lengths) / len(list_lengths)
+        print(f"Average number of potential matches per amendment: {average_length}")
+        return self.similar_doc_indices
+
+    def find_best_matches(
+        self,
+        column_used_for_comparison: str = "Exposé amdt",
+        threshold_ratio: float = 0.95,
+    ) -> dict:
+        if self.similar_doc_indices is None:
+            raise ValueError(
+                "You need to prefilter similar documents (with `prefilter_similar_docs`) before finding the best matches."
+            )
+        old_amendments = {
+            "text": self.old_amendments_df[column_used_for_comparison],
+            "comparison_value": -self.old_amendments_df["Year"],
+        }
+        new_amendments = self.new_amendments_df[column_used_for_comparison]
+        print("Looking for best matches on pre-filtered amendments...")
+        closest_docs = ContentSimilarityEvaluator.find_best_matching_docs(
+            similar_doc_indices=self.similar_doc_indices,
+            left_docs=new_amendments,
+            right_docs=old_amendments,
+            threshold_ratio=threshold_ratio,
+        )
+        print(f"Found matches in previous PLFSS for {len(closest_docs)} amendments")
+        return closest_docs
---
+++ tests/unit/clustering/test_similarity_finder.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import pytest
+
+from amendements_intelligents.clustering.similarity_finder import SimilarityFinder
+
+
+@pytest.mark.parametrize(
+    "old_amendments_df, new_amendments_df",
+    [
+        (
+            pd.DataFrame(
+                {
+                    "Exposé amdt": [
+                        "Cet amendement aborde le ABC et le DEF",
+                        "Cet amendement aborde le GHI",
+                        "Cet amendement aborde le JKL et le MNO",
+                        "Un amendement qui n'a rien à voir",
+                    ],
+                }
+            ),
+            pd.DataFrame(
+                {
+                    "Exposé amdt": [
+                        "Cet amendement aborde le JKL",
+                        "Cet amendement aborde le ABC et le DEF",
+                        "no match",
+                        "Cet amendement aborde le DEF",
+                    ],
+                }
+            ),
+        )
+    ],
+)
+def test_prefilter_similar_docs(old_amendments_df, new_amendments_df):
+    similarity_finder = SimilarityFinder(old_amendments_df, new_amendments_df)
+    similar_doc_indices = similarity_finder.prefilter_similar_docs()
+    assert similar_doc_indices == {0: [2], 1: [0], 3: [0]}
+
+
+@pytest.mark.parametrize(
+    "old_amendments_df, new_amendments_df",
+    [
+        (
+            pd.DataFrame(
+                {
+                    "Exposé amdt": [
+                        "Cet amendement aborde le ABC et le DEF",
+                        "Cet amendement aborde le GHI",
+                        "Cet amendement aborde le ABC et le DEF",
+                        "Un amendement qui n'a rien à voir",
+                    ],
+                    "Year": [3000, 2021, 2021, 2021],
+                }
+            ),
+            pd.DataFrame(
+                {
+                    "Exposé amdt": [
+                        "Cet amendement aborde le JKL",
+                        "Cet amendement aborde le ABC et le DEF",
+                        "no match",
+                        "Cet amendement aborde le DEF",
+                    ],
+                    "Year": [2023, 2023, 2023, 2023],
+                }
+            ),
+        )
+    ],
+)
+def test_find_best_matches(old_amendments_df, new_amendments_df):
+    similarity_finder = SimilarityFinder(old_amendments_df, new_amendments_df)
+    similarity_finder.prefilter_similar_docs()
+    closest_docs = similarity_finder.find_best_matches(threshold_ratio=0.7)
+    assert closest_docs == {
+        1: {
+            "best_matching_comparison_value": -3000,
+            "best_matching_doc_idx": 0,
+            "best_matching_doc_length": 38,
+            "similarity_ratio": 1.0,
+        },
+        3: {
+            "best_matching_comparison_value": -3000,
+            "best_matching_doc_idx": 0,
+            "best_matching_doc_length": 38,
+            "similarity_ratio": 0.7368421052631579,
+        },
+    }
+
+
+def test_find_best_matches_without_prefilter():
+    similarity_finder = SimilarityFinder(None, None)
+    with pytest.raises(ValueError):
+        similarity_finder.find_best_matches()
