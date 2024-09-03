25650fc6a78ab57f79d5e39b876fea1059fe6884
Victor DEGLIAME <victor.degliame@gmail.com>
2024-09-03 17:51:01 +0200

    feat: Add attribution to full pipeline

--- amendements_intelligents/full_pipeline.py
+++ amendements_intelligents/full_pipeline.py
@@ -1,18 +1,26 @@
 import os
+import re
 
+import pandas as pd
+
+from amendements_intelligents.attribution.attribution_data_loader import (
+    AttributionDataLoader,
+)
+from amendements_intelligents.attribution.plfss_attributor import PLFSSAttributor
 from amendements_intelligents.populate_allotments import PLFSSAllotmentPopulator
 from amendements_intelligents.populate_summaries import AmendmentSummaryPopulator
 from amendements_intelligents.summary.llm_clients import FakeLLMAPIClient, LLMAPIClient
 from amendements_intelligents.utils.plfss_pre_processor import PLFSSPreProcessor
-
-MODEL_NAME = os.getenv("MODEL_NAME")
-DATA_FOLDER = os.getenv("DATA_FOLDER")
-VLLM_ENDPOINT = os.getenv("VLLM_ENDPOINT")
-USER = os.getenv("USER")
-PASSWORD = os.getenv("PASSWORD")
+from amendements_intelligents.utils.plfss_text_utils import AttributionTextNormalizer
 
 
 def main():
+    DATA_FOLDER = os.getenv("DATA_FOLDER")
+    OUTPUT_FILE = f"{DATA_FOLDER}/full_pipeline_df.xlsx"
+    # MODEL_NAME = os.getenv("MODEL_NAME")
+    # VLLM_ENDPOINT = os.getenv("VLLM_ENDPOINT")
+    # USER = os.getenv("USER")
+    # PASSWORD = os.getenv("PASSWORD")
     # llm_api_client: LLMAPIClient = LLMInferenceAPIClient(
     #     url="http://localhost:8000/generate"
     # )
@@ -20,6 +28,7 @@ def main():
     # BEGIN LOAD AND PRE-PROCESS DATA
     plfss_input_file = (f"{DATA_FOLDER}/PLFSS_2024.json", 2024)
     acronym_file = f"{DATA_FOLDER}/acronym_mapping.xlsx"
+    mappings_file = f"{DATA_FOLDER}/mappings_attributions_aug_9.xlsx"
     SUMMARY_COLUMN = "Objet"
 
     preprocessor = PLFSSPreProcessor
@@ -35,7 +44,6 @@ def main():
     cleaned_original_amdt_df = PLFSSPreProcessor.prepare_amendments_columns(
         amendments_df=amendments_df
     )
-
     # END LOAD AND PRE-PROCESS DATA
 
     # BEGIN SUMMARY GENERATION
@@ -51,7 +59,6 @@ def main():
     # END SUMMARY GENERATION
 
     # BEGIN PRE-PROCESS FOR ALLOTMENTS
-
     prepared_for_alot_df = PLFSSPreProcessor.remove_empty_rows_for_given_columns(
         amendments_df=amdt_with_summaries_df, columns_to_filter_with=["Corps amdt"]
     )
@@ -61,7 +68,6 @@ def main():
     prepared_for_alot_df = PLFSSPreProcessor.normalize_plfss(
         amendments_df=prepared_for_alot_df, columns_to_normalize=["Corps amdt"]
     )
-
     # END PRE-PROCESS FOR ALLOTMENTS
 
     # BEGIN ALLOTMENTS
@@ -71,7 +77,44 @@ def main():
     )
     # END ALLOTMENTS
 
-    amdt_with_allotments_df.to_excel("data/amdt_with_allotments_and_object_df.xlsx")
+    # BEGIN ATTRIBUTION
+    amdt_with_attribution_df = amdt_with_allotments_df.copy()
+    amdt_with_attribution_df["Corps amdt"] = amdt_with_allotments_df[
+        "Corps amdt orig"
+    ].apply(lambda x: AttributionTextNormalizer.normalize_text(str(x)))
+    result_df = amdt_with_attribution_df
+
+    attribution_mappings_excel = pd.read_excel(mappings_file, sheet_name=None)
+    codes_articles_df = AttributionDataLoader.load_codes_and_articles(
+        attribution_mappings_excel
+    )
+    keywords_df = AttributionDataLoader.load_keywords(attribution_mappings_excel)
+
+    codes_set = set(codes_articles_df["Code"])
+    max_code_length = codes_articles_df["Code"].str.len().max()
+    articles_set = set(codes_articles_df["Articles"])
+    pattern = re.compile(r"(?:\d+(?:-\d+)*)(?:\s(.+))?")
+    latin_ordinals_set = {
+        match.group(1)
+        for article in articles_set
+        if (match := pattern.match(article)) and match.group(1)
+    }
+
+    attributor = PLFSSAttributor(
+        amendments_df=amdt_with_attribution_df,
+        articles_set=articles_set,
+        codes_articles_df=codes_articles_df,
+        codes_set=codes_set,
+        keywords_df=keywords_df,
+        latin_ordinals_set=latin_ordinals_set,
+        max_code_length=max_code_length,
+    )
+    amdt_with_attribution_df = attributor.populate()
+    result_df = amdt_with_attribution_df
+    # END ATTRIBUTION
+
+    result_df.to_excel(OUTPUT_FILE)
+    print(f"Saved amendment with attribution, allotments and object to: {OUTPUT_FILE}")
 
 
 if __name__ == "__main__":
