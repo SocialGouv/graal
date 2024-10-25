7f97602d0e5896c42516201ccec83dcea435e5db
Victor DEGLIAME <victor.degliame@gmail.com>
2024-10-25 15:52:43 +0200

    feat: Use several clients for summary generation

--- amendements_intelligents/full_pipeline.py
+++ amendements_intelligents/full_pipeline.py
@@ -83,6 +83,7 @@ def main():
         "amdt_idx",
     ]
 
+    llm_api_clients = []
     # fake_llm_api_client: LLMAPIClient = FakeLLMAPIClient()
 
     # llm_api_client = VllmAPIClient(
@@ -96,24 +97,29 @@ def main():
     OLLAMA_PASSWORD = os.getenv("OLLAMA_PASSWORD")
     OLLAMA_ENDPOINT = os.getenv("OLLAMA_ENDPOINT")
     OLLAMA_MODEL_NAME = os.getenv("OLLAMA_MODEL_NAME")
-    ollama_api_client = OllamaAPIClient(
-        endpoint=OLLAMA_ENDPOINT,
-        model_name=OLLAMA_MODEL_NAME,
-        user=OLLAMA_USER,
-        password=OLLAMA_PASSWORD,
-    )
 
-    albert_api_client: LLMAPIClient = EtalabAPIClient(
-        base_url=os.getenv("ETALAB_BASE_URL", "https://albert.api.etalab.gouv.fr/v1"),
-        api_key=os.getenv("ETALAB_API_KEY"),
-        model_name=os.getenv(
-            "ETALAB_MODEL_NAME", "meta-llama/Meta-Llama-3.1-70B-Instruct"
-        ),
-    )
+    for _ in range(10):
+        ollama_api_client = OllamaAPIClient(
+            endpoint=OLLAMA_ENDPOINT,
+            model_name=OLLAMA_MODEL_NAME,
+            user=OLLAMA_USER,
+            password=OLLAMA_PASSWORD,
+        )
+        llm_api_clients.append(ollama_api_client)
 
-    summary_gen_load_balancer = SummaryGenerationLoadBalancer(
-        clients=[albert_api_client, ollama_api_client]
-    )
+    for _ in range(6):
+        albert_api_client: LLMAPIClient = EtalabAPIClient(
+            base_url=os.getenv(
+                "ETALAB_BASE_URL", "https://albert.api.etalab.gouv.fr/v1"
+            ),
+            api_key=os.getenv("ETALAB_API_KEY"),
+            model_name=os.getenv(
+                "ETALAB_MODEL_NAME", "meta-llama/Meta-Llama-3.1-70B-Instruct"
+            ),
+        )
+        llm_api_clients.append(albert_api_client)
+
+    summary_gen_load_balancer = SummaryGenerationLoadBalancer(clients=llm_api_clients)
 
     # BEGIN LOAD AND PRE-PROCESS DATA
     amendments_df = AmendmentPreProcessor.load_amendments_json(input_files=[INPUT_FILE])
