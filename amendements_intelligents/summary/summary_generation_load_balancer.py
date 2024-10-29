0d777b3cd9ecfc0292da612224c9533ee81ec60f
Victor DEGLIAME <victor.degliame@gmail.com>
2024-10-29 18:50:06 +0100

    chore: Better handling of llm clients in case of failures

--- amendements_intelligents/summary/summary_generation_load_balancer.py
+++ amendements_intelligents/summary/summary_generation_load_balancer.py
@@ -7,15 +7,26 @@ from amendements_intelligents.summary.llm_clients import LLMAPIClient
 
 
 class SummaryGenerationLoadBalancer:
-    def __init__(self, clients: List[LLMAPIClient], max_retries: int = 3):
+    def __init__(
+        self,
+        clients: List[LLMAPIClient],
+        queue_timeout: float,
+        max_retries: int = 3,
+    ):
         self.client_pool: queue.Queue[LLMAPIClient] = queue.Queue()
         for client in clients:
             self.client_pool.put(client)
         self.max_retries = max_retries
         self.summary_count = 0  # Initialize summary count
+        self.queue_timeout = queue_timeout
 
     def _obtain_client(self) -> LLMAPIClient:
-        return self.client_pool.get()
+        try:
+            return self.client_pool.get(timeout=self.queue_timeout)
+        except queue.Empty as e:
+            raise TimeoutError(
+                "No available clients in the pool within the timeout period."
+            ) from e
 
     def _put_client_back(self, client: LLMAPIClient):
         self.client_pool.put(client)
@@ -23,25 +34,27 @@ class SummaryGenerationLoadBalancer:
     def generate_summary(self, prompt: str) -> str:
         retries = 0
         while retries < self.max_retries:
-            client = self._obtain_client()
+            try:
+                client = self._obtain_client()
+                logging.warning(f"Using client {client.name}")
+            except TimeoutError as e:
+                logging.error(e)
+                raise TimeoutError(
+                    "Failed to obtain client within timeout period."
+                ) from e
+
             try:
                 result = client.generate_summary(prompt)
                 self._put_client_back(client)
                 self.summary_count += 1  # Increment summary count
                 if self.summary_count % 10 == 0:
-                    print(f"Summaries generated: {self.summary_count}")
+                    logging.info(f"Summaries generated: {self.summary_count}")
                 return result
             except Exception as e:
                 logging.error(f"Error with client {client.name}: {e}")
+                self._put_client_back(client)
                 retries += 1
-                if retries < self.max_retries:
-                    self._put_client_back(client)
-                else:
-                    logging.warning(
-                        f"Removing client {client} from pool after {self.max_retries} retries."
-                    )
-                    retries = 0
-        return "All clients failed after retries."
+        raise RuntimeError("All llm clients failed after retries.")
 
     def generate_summaries_concurrent(self, prompts: List[str]) -> List[str]:
         with ThreadPoolExecutor(max_workers=len(prompts)) as executor:
