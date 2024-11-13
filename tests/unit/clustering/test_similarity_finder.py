362ebf88433388285222dc91378af618890ae427
Victor DEGLIAME <victor.degliame@gmail.com>
2024-11-13 16:13:10 +0100

    refactor: Reorganize similarity thresholds a little

--- tests/unit/clustering/test_similarity_finder.py
+++ tests/unit/clustering/test_similarity_finder.py
@@ -27,10 +27,10 @@ def test_prefilter_similar_docs():
             "amdt_idx": [5, 6, 7, 8],
         }
     )
-    similarity_finder = SimilarityFinder(
-        old_amendments_df, new_amendments_df, exact_match_threshold_ratio=0.7
+    similarity_finder = SimilarityFinder(old_amendments_df, new_amendments_df)
+    similar_doc_indices = similarity_finder.prefilter_similar_docs(
+        clustering_similarity_threshold=0.7
     )
-    similar_doc_indices = similarity_finder.prefilter_similar_docs(threshold=0.7)
     assert similar_doc_indices == {5: [3], 6: [1], 8: [1]}
 
 
@@ -72,11 +72,11 @@ def test_find_best_matches():
     similarity_finder = SimilarityFinder(
         old_amendments_df=old_amendments_df,
         new_amendments_df=new_amendments_df,
-        exact_match_threshold_ratio=0.7,
     )
     similarity_finder.prefilter_similar_docs()
     closest_docs = similarity_finder.find_best_matches(
-        column_used_for_similarity="Exposé amdt"
+        column_used_for_similarity="Exposé amdt",
+        exact_match_similarity_threshold=0.7,
     )
     assert closest_docs == {
         6: {
@@ -97,7 +97,10 @@ def test_find_best_matches():
 def test_find_best_matches_missing_df():
     similarity_finder = SimilarityFinder(None, None)
     with pytest.raises(TypeError):
-        similarity_finder.find_best_matches()
+        similarity_finder.find_best_matches(
+            column_used_for_similarity="Exposé amdt",
+            exact_match_similarity_threshold=0.7,
+        )
 
 
 def test_find_best_matching_docs():
@@ -132,8 +135,8 @@ def test_find_best_matching_docs():
         similar_doc_indices=similar_doc_indices,
         new_amdt_data=new_amdt_data,
         old_amdt_data=old_amdt_data,
-        exact_match_threshold_ratio=default_threshold_ratio,
-        threshold_ratio_mappings=threshold_ratio_mappings,
+        exact_match_similarity_threshold=default_threshold_ratio,
+        similarity_threshold_overrides=threshold_ratio_mappings,
     )
 
     assert closest_docs == expected_output
@@ -177,8 +180,8 @@ def test_find_best_matching_docs_with_preference_for_non_empty_responses():
         similar_doc_indices=similar_doc_indices,
         new_amdt_data=new_amdt_data,
         old_amdt_data=old_amdt_data,
-        exact_match_threshold_ratio=default_threshold_ratio,
-        threshold_ratio_mappings=threshold_ratio_mappings,
+        exact_match_similarity_threshold=default_threshold_ratio,
+        similarity_threshold_overrides=threshold_ratio_mappings,
     )
 
     assert closest_docs == expected_output
