ddacaa80399187b49f5bc4b9a02d3552e2ec42e9
Victor DEGLIAME <victor.degliame@gmail.com>
2024-12-03 00:33:54 +0100

    tests: Add a unit test for SimilarityHandler.populate

--- tests/unit/clustering/test_similarity_handler.py
+++ tests/unit/clustering/test_similarity_handler.py
@@ -1,5 +1,7 @@
+import logging
 import textwrap
 
+import numpy as np
 import pandas as pd
 import pytest
 
@@ -16,9 +18,6 @@ def sample_data():
             "Objet amdt": ["Objet 1", "Objet 2", "Objet 3"],
             "Organe": ["Organe 1", "Organe 2", "Organe 3"],
             "Réponse": ["Response 1", "Response 2", "Response 3"],
-            "Corps amdt orig": ["Corps 1", "Corps 2", "Corps 3"],
-            "Exposé amdt orig": ["Exposé 1", "Exposé 2", "Exposé 3"],
-            "Objet orig": ["Objet 1", "Objet 2", "Objet 3"],
             "Sort": ["Sort 1", "Irrecevable 123", "Sort 3"],
             "origin_project": ["PLFSS", "PLACSS", "PLFSS"],
         }
@@ -49,8 +48,6 @@ def sample_data():
             "Lecture": ["A", "B", "C"],
             "Réponse": ["", "", ""],
             "Commentaires": ["", "", ""],
-            "Corps amdt trouvé": ["", "", ""],
-            "Exposé amdt trouvé": ["", "", ""],
             "Sort": ["", "", ""],
         }
     )
@@ -104,3 +101,97 @@ def test_copy_matches_to_amendments_df(sample_data):
         """).strip()
     )
     assert result_df.loc[2, "Sort"] == ""
+
+
+def test_populate():
+    preprocessed_old_amendments_df = pd.DataFrame(
+        {
+            "Num amdt": [1, 2, 3],
+            "amdt_idx": [1, 2, 3],
+            "Lecture": ["A", "B", "C"],
+            "Objet amdt": ["Objet 1", "Objet 2", "Objet 3"],
+            "Organe": ["Organe 1", "Organe 2", "Organe 3"],
+            "Réponse": ["Response 1", "Response 2", "Response 3"],
+            "Sort": ["Sort 1", "Irrecevable 123", "Sort 3"],
+            "origin_project": ["PLFSS", "PLACSS", "PLFSS"],
+            "Exposé amdt": ["Exposé 1", "Exposé 2", "Exposé 3"],
+            "Corps amdt": ["Corps 1", "Corps 2", "Corps 3"],
+            "timestamp": [0, 0, 0],
+        }
+    )
+
+    preprocessed_new_amendments_df = pd.DataFrame(
+        {
+            "Num amdt": [1, 2, 3],
+            "amdt_idx": [97, 98, 99],
+            "Lecture": ["A", "B", "C"],
+            "Objet amdt": ["Objet 1", "Objet 2", "Objet 3"],
+            "Organe": ["Organe 1", "Organe 2", "Organe 3"],
+            "Exposé amdt": ["Exposé 1", "Exposé 2", "Not the same Exposé 3"],
+            "Corps amdt": ["Corps 1", "Corps 2", "Corps 3"],
+            "timestamp": [0, 0, 0],
+            "Commentaires": ["", "", ""],
+        }
+    )
+
+    original_new_amendments_df = preprocessed_new_amendments_df.copy()
+
+    clustering_similarity_thresholds = {
+        "Exposé amdt": 1,
+        "Corps amdt": 1,
+    }
+    fuzzy_match_similarity_thresholds = {
+        "Exposé amdt": 1,
+        "Corps amdt": 1,
+    }
+    similarity_threshold_overrides = {}
+
+    result_df = SimilarityHandler.populate(
+        preprocessed_old_amendments_df=preprocessed_old_amendments_df,
+        preprocessed_new_amendments_df=preprocessed_new_amendments_df,
+        original_new_amendments_df=original_new_amendments_df,
+        clustering_similarity_thresholds=clustering_similarity_thresholds,
+        fuzzy_match_similarity_thresholds=fuzzy_match_similarity_thresholds,
+        similarity_threshold_overrides=similarity_threshold_overrides,
+    )
+
+    logging.warning(f"result_df {result_df}")
+    assert (
+        result_df.loc[0, "Commentaires"]
+        == textwrap.dedent("""
+        Réponse copiée de : PLFSS 1970
+        Numéro d'amendement : 1
+        Lecture : A
+        Organe : Organe 1
+        Colonne similaire : Exposé amdt
+        """).strip()
+    )
+    assert (
+        result_df.loc[1, "Commentaires"]
+        == textwrap.dedent("""
+        Réponse copiée de : PLACSS 1970
+        Numéro d'amendement : 2
+        Lecture : B
+        Organe : Organe 2
+        Colonne similaire : Exposé amdt
+        Sort copié : Irrecevable 123
+        """).strip()
+    )
+    assert (
+        result_df.loc[2, "Commentaires"]
+        == textwrap.dedent("""
+        Réponse copiée de : PLFSS 1970
+        Numéro d'amendement : 3
+        Lecture : C
+        Organe : Organe 3
+        Colonne similaire : Corps amdt
+        """).strip()
+    )
+
+    assert result_df.loc[0, "Réponse"] == "Response 1"
+    assert result_df.loc[1, "Réponse"] == "Response 2"
+    assert result_df.loc[2, "Réponse"] == "Response 3"
+
+    assert result_df.loc[0, "Sort"] is np.nan
+    assert result_df.loc[1, "Sort"] == "Irrecevable 123"
+    assert result_df.loc[2, "Sort"] is np.nan
