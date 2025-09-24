0269af4b34e3aad50c5fda07fe46f68422508eb1
Victor DEGLIAME <victor.degliame@gmail.com>
2025-09-24 18:04:32 +0200

    config: Ensure monorepo works for backend + frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { startReactDsfr } from "@codegouvfr/react-dsfr/spa"
 import './index.css'
-import App from './App.tsx'
+import App from './App'
 
 startReactDsfr({
   defaultColorScheme: "system",
