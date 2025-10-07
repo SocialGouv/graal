b1b5dc3dc5f9d29a26e30a827a37eda413afdced
Victor DEGLIAME <victor.degliame@gmail.com>
2025-10-07 17:50:10 +0200

    feat: Add columns to copy and threshold slider components
    vidy-br: add-config-components

--- frontend/src/components/FeatureConfig/index.ts
+++ frontend/src/components/FeatureConfig/index.ts
@@ -2,6 +2,8 @@ export { FeatureConfigSection } from './FeatureConfigSection';
 export { ColumnSimilarityConfig } from './ColumnSimilarityConfig';
 export { SimpleToggleConfig } from './SimpleToggleConfig';
 export { ProjectSelectionConfig } from './ProjectSelectionConfig';
+export { ThresholdSliderConfig } from './ThresholdSliderConfig';
+export { ColumnsToCopyConfig } from './ColumnsToCopyConfig';
 
 export type { ColumnOption, ColumnSimilarityConfigProps } from './ColumnSimilarityConfig';
 export type { SimpleToggleConfigProps } from './SimpleToggleConfig';
